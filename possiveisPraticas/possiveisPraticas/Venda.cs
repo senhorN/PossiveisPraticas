59b1ad111c448702accf9615a6cdc0d3ac67e3fe
Nicolas Carloto <90424448+senhorN@users.noreply.github.com>
2022-12-16 14:33:28 -0300

    Add files via upload

---
+++ possiveisPraticas/possiveisPraticas/Venda.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace possiveisPraticas
+{
+    class Venda
+    {
+        public Venda(int id, string produto, decimal preco)
+        {
+            id = id;
+            produto = produto;
+            preco = preco;
+        }
+        public int id { get; set; }
+        public string produto { get; set; }
+        public decimal preco { get; set; }
+    }
+}
